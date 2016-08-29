f1a7779673a76d9e20ed23b1d403e602859001ac
JuZiWang <459362047@qq.com>
2016-08-29 20:52:40 +0800

    Signed-off-by: JuZiWang <459362047@qq.com>

---
+++ NewGit/app/src/main/java/com/wj/daisy/newgit/Bean.java
@@ -0,0 +1,13 @@
+package com.wj.daisy.newgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bean extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bean);
+    }
+}
